804a6d15bda128d1e39fb5172078c4fc2750a86e
jution113 <jution113@gmail.com>
2025-03-03 21:27:12 +0900

    init: create project using spirng initializer

---
+++ Avenir/src/main/java/juton113/Avenir/AvenirApplication.java
@@ -0,0 +1,13 @@
+package juton113.Avenir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvenirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvenirApplication.class, args);
+	}
+
+}
---
+++ Avenir/src/test/java/juton113/Avenir/AvenirApplicationTests.java
@@ -0,0 +1,13 @@
+package juton113.Avenir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvenirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
