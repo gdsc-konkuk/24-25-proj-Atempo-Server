f73aca548a47e7935e7e7374a35801988e12608d
jution113 <jution113@gmail.com>
2025-03-07 16:30:55 +0900

    chore: chang print content in Application class for dploy test

--- Avenir/src/main/java/juton113/Avenir/AvenirApplication.java
+++ Avenir/src/main/java/juton113/Avenir/AvenirApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AvenirApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello Avenir! 2");
+		System.out.println("hello Avenir! 3");
 		SpringApplication.run(AvenirApplication.class, args);
 	}
 
