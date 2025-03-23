4e98b31632ce371e91cfd562d2e8ee60ba64ceb7
jution113 <jution113@gmail.com>
2025-03-23 22:29:43 +0900

    feat: add JwtAuthenticationFilter class

---
+++ Avenir/src/main/java/juton113/Avenir/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package juton113.Avenir.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import juton113.Avenir.domain.enums.TokenType;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = tokenService.resolveToken(request, TokenType.ACCESS);
+
+        if(accessToken != null) {
+            Claims claims = tokenService.parseToken(accessToken);
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
