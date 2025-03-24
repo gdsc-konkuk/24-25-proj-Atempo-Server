f9abc106362bd34b8f31d077a5f1931197efce4a
jution113 <jution113@gmail.com>
2025-03-24 20:53:37 +0900

    docs: add swagger comment in auth api

--- Avenir/src/main/java/juton113/Avenir/controller/AuthController.java
+++ Avenir/src/main/java/juton113/Avenir/controller/AuthController.java
@@ -1,8 +1,9 @@
 package juton113.Avenir.controller;
 
 import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import juton113.Avenir.domain.dto.LoginResponseDto;
 import juton113.Avenir.domain.dto.TokenResponseDto;
 import juton113.Avenir.domain.enums.TokenType;
@@ -12,10 +13,14 @@ import juton113.Avenir.service.RedisService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@Tag(name = "인증, 인가", description = "로그인, 토큰 발급과 관련된 API")
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/auth")
@@ -24,6 +29,7 @@ public class AuthController {
     private final RedisService redisService;
     private final TokenService tokenService;
 
+    @Operation(summary = "로그인 요청", description = "Oauth 로그인 url 요청")
     @GetMapping("/login")
     public ResponseEntity<LoginResponseDto> login() {
         String loginUrl = "/oauth2/authorization/google";
@@ -31,6 +37,7 @@ public class AuthController {
         return ResponseEntity.ok(new LoginResponseDto("OAuth 로그인 URL 반환", loginUrl));
     }
 
+    @Operation(summary = "토큰 재발급", description = "Access Token과 Refresh Token을 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<?> reissueToken(HttpServletRequest request) {
         String refreshToken = tokenService.resolveToken(request, TokenType.REFRESH);
