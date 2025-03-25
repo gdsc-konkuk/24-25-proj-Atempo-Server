886b5ece1c2cea3fa41484c8678ca1b23c94cca6
jution113 <jution113@gmail.com>
2025-03-25 17:23:47 +0900

    refactor: remove unused code in SwaggerConfig

--- Avenir/src/main/java/juton113/Avenir/config/SwaggerConfig.java
+++ Avenir/src/main/java/juton113/Avenir/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package juton113.Avenir.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
@@ -11,17 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(title = "API 문서", version = "v1")
 )
+
 @SecurityScheme(
-        name = "Access Token Auth",
+        name = "JWT Auth",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
         bearerFormat = "JWT"
 )
-@SecurityScheme(
-        name = "Refresh Token Auth",
-        type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.HEADER,
-        paramName = "X-Refresh-Token"
-)
+
 public class SwaggerConfig {
 }
