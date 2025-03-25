256565b8468160ee8441a52fc00ca93aaccdfc1b
jution113 <jution113@gmail.com>
2025-03-25 17:10:13 +0900

    feat: split token creating logic to AuthController

--- Avenir/src/main/java/juton113/Avenir/controller/AuthController.java
+++ Avenir/src/main/java/juton113/Avenir/controller/AuthController.java
@@ -7,7 +7,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import juton113.Avenir.domain.dto.AccessTokenDto;
 import juton113.Avenir.domain.dto.LoginResponseDto;
+import juton113.Avenir.domain.dto.RefreshTokenDto;
 import juton113.Avenir.domain.dto.TokenResponseDto;
 import juton113.Avenir.domain.enums.TokenType;
 import juton113.Avenir.security.jwt.TokenService;
@@ -22,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
+
+import static juton113.Avenir.domain.enums.TokenType.ACCESS;
+import static juton113.Avenir.domain.enums.TokenType.REFRESH;
 
 @Tag(name = "인증, 인가", description = "로그인, 토큰 발급과 관련된 API")
 @AllArgsConstructor
@@ -59,16 +64,22 @@ public class AuthController {
                     )
             }
     )
-    @PostMapping("/reissue")
-    public ResponseEntity<?> reissueToken(HttpServletRequest request) {
-        String refreshToken = tokenService.resolveToken(request, TokenType.REFRESH);
+    @PostMapping("/access-token")
+    public ResponseEntity<?> reissueAccessToken(HttpServletRequest request) {
+        String refreshToken = tokenService.resolveToken(request);
 
         if (refreshToken == null) {
             // TODO: tokenService.resolveToken 내에 예외 처리 로직 작성 후 지울 것
-            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token");
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token - Empty");
         }
 
         Claims claims = tokenService.parseToken(refreshToken);
+
+        if (claims.get("tokenType") != REFRESH) {
+            // TODO: tokenService.resolveToken 내에 예외 처리 로직 작성 후 지울 것
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token - Different");
+        }
+
         String memberId = claims.getSubject();
 
         Optional<String> optionalToken = redisService.getToken(memberId);
@@ -77,13 +88,42 @@ public class AuthController {
             return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token");
         }
 
-        TokenResponseDto tokenResponseDto = authService.issueToken(memberId);
-        String newRefreshToken = tokenResponseDto.getRefreshToken();
+        AccessTokenDto accessTokenDto = authService.issueAccessToken(memberId);
 
         return ResponseEntity.ok()
-                .header("Authorization", "Bearer " + tokenResponseDto.getAccessToken())
-                .header("X-Refresh-Token", "Bearer " + newRefreshToken)
-                .body("Token reissued");
+                .header("Authorization", "Bearer " + accessTokenDto.getAccessToken())
+                .body("Access Token reissued");
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> reissueRefreshToken(HttpServletRequest request) {
+        String refreshToken = tokenService.resolveToken(request);
+
+        if (refreshToken == null) {
+            // TODO: tokenService.resolveToken 내에 예외 처리 로직 작성 후 지울 것
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token - Empty");
+        }
+
+        Claims claims = tokenService.parseToken(refreshToken);
+
+        if (claims.get("tokenType") != REFRESH) {
+            // TODO: tokenService.resolveToken 내에 예외 처리 로직 작성 후 지울 것
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token - Different");
+        }
+
+        String memberId = claims.getSubject();
+
+        Optional<String> optionalToken = redisService.getToken(memberId);
+        if(optionalToken.isEmpty() || !optionalToken.get().equals(refreshToken)) {
+            // TODO: redisService.getToken 내에 예외 처리 로직 작성 후 지울 것
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid Token");
+        }
+
+        RefreshTokenDto refreshTokenDto = authService.issueRefreshToken(memberId);
+
+        return ResponseEntity.ok()
+                .header("Authorization", "Bearer " + refreshTokenDto.getRefreshToken())
+                .body("Refresh Token reissued");
     }
 
 }
