4426b0e43ce7f4a49ca9859fb9d948a7ad263075
jution113 <jution113@gmail.com>
2025-03-26 22:01:24 +0900

    feat:  add token validating logic in AuthService class for MVC paradigm

--- Avenir/src/main/java/juton113/Avenir/service/AuthService.java
+++ Avenir/src/main/java/juton113/Avenir/service/AuthService.java
@@ -1,7 +1,9 @@
 package juton113.Avenir.service;
 
+import io.jsonwebtoken.Claims;
 import juton113.Avenir.domain.dto.AccessTokenDto;
 import juton113.Avenir.domain.dto.RefreshTokenDto;
+import juton113.Avenir.domain.enums.TokenType;
 import juton113.Avenir.security.jwt.TokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,16 +14,27 @@ public class AuthService {
     private final TokenService tokenService;
     private final RedisService redisService;
 
-    public AccessTokenDto issueAccessToken(String subject) {
-        String accessToken = tokenService.createAccessToken(subject);
+    public AccessTokenDto issueAccessToken(String refreshToken) {
+        Claims claims = tokenService.parseToken(refreshToken);
+        tokenService.validateTokenType(claims, TokenType.REFRESH);
+
+        String memberId = claims.getSubject();
+        redisService.validateStoredToken(memberId, refreshToken);
+
+        String accessToken = tokenService.createAccessToken(memberId);
 
         return new AccessTokenDto(accessToken);
     }
 
-    public RefreshTokenDto issueRefreshToken(String subject) {
-        String refreshToken = tokenService.createRefreshToken(subject);
-        redisService.saveToken(subject, refreshToken);
+    public RefreshTokenDto issueRefreshToken(String refreshToken) {
+        Claims claims = tokenService.parseToken(refreshToken);
+        tokenService.validateTokenType(claims, TokenType.REFRESH);
 
-        return new RefreshTokenDto(refreshToken);
+        String memberId = claims.getSubject();
+        redisService.validateStoredToken(memberId, refreshToken);
+
+        String newRefreshToken = tokenService.createRefreshToken(memberId);
+
+        return new RefreshTokenDto(newRefreshToken);
     }
 }
