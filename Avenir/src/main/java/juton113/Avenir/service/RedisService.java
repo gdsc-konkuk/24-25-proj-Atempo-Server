e8a5520d986b217c26b8e5c324f685f4012afc40
jution113 <jution113@gmail.com>
2025-03-26 21:54:32 +0900

    feat: add refresh token validate logic to read token method in RedisService class

--- Avenir/src/main/java/juton113/Avenir/service/RedisService.java
+++ Avenir/src/main/java/juton113/Avenir/service/RedisService.java
@@ -1,5 +1,7 @@
 package juton113.Avenir.service;
 
+import juton113.Avenir.domain.enums.ErrorCode;
+import juton113.Avenir.exception.CustomException;
 import juton113.Avenir.security.jwt.TokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,17 +18,20 @@ public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
 
     public void saveToken(String key, String refreshToken) {
-        // TODO: 중복 검사를 적용할 지 고려할 것
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
         valueOperations.set(key, refreshToken, tokenService.getRefreshTokenTime(), TimeUnit.MILLISECONDS);
     }
 
-    public Optional<String> getToken(String key) {
-        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
-        return Optional.ofNullable(valueOperations.get(key));
-    }
+        public void validateStoredToken(String key, String token) {
+            ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
 
-    public void deleteToken(String key) {
+            String foundToken = Optional.of(valueOperations.get(key)).orElseThrow(()-> new CustomException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
+
+            if(!foundToken.equals(token))
+                throw new CustomException(ErrorCode.REFRESH_TOKEN_NOT_FOUND);
+        }
+
+        public void deleteToken(String key) {
         // TODO: 검증 후 삭제할 수 있도록
         redisTemplate.delete(key);
     }
