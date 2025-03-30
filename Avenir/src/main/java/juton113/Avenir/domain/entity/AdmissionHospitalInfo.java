f2ef7f087daa57d28e112ce4ed774a27f6ba2c3f
jution113 <jution113@gmail.com>
2025-03-31 08:53:51 +0900

    feat: add travel time, distance, detail field  to AdmissionHospitalInfo

--- Avenir/src/main/java/juton113/Avenir/domain/entity/AdmissionHospitalInfo.java
+++ Avenir/src/main/java/juton113/Avenir/domain/entity/AdmissionHospitalInfo.java
@@ -8,20 +8,29 @@ import lombok.AllArgsConstructor;
 @Entity
 @AllArgsConstructor
 @Table(name = "admission_hospital_response")
-public class AdmissionHospitalResponse {
+public class AdmissionHospitalInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admission_request_id", nullable = false)
-    AdmissionRequest admissionRequest;
+    Admission admission;
 
     @Column(nullable = false)
     String hospitalName;
 
     @Column(nullable = false)
     String hospitalPhoneNumber;
+
+    @Column(nullable = false)
+    int distance; // TODO: 단위 정하기
+
+    @Column(nullable = false)
+    int travelTime; // TODO: 단위 정하기
+
+    @Column(nullable = false)
+    String detail; // TODO: 어떤 내용을 넣을 지 정하기
 
     @Enumerated(EnumType.STRING)
     CallStatus callStatus;
