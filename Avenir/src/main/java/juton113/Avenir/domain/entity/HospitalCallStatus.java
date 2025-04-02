0bae13676f1aa869680a36873088fc4c4d2b2f95
jution113 <jution113@gmail.com>
2025-04-02 17:06:19 +0900

    refactor: rename and hostal fk in HospitalCallStatus clas

--- Avenir/src/main/java/juton113/Avenir/domain/entity/HospitalCallStatus.java
+++ Avenir/src/main/java/juton113/Avenir/domain/entity/HospitalCallStatus.java
@@ -2,29 +2,16 @@ package juton113.Avenir.domain.entity;
 
 import jakarta.persistence.*;
 import juton113.Avenir.domain.enums.CallResponseStatus;
+import juton113.Avenir.domain.enums.CallStatus;
 import lombok.AllArgsConstructor;
 
 @Entity
 @AllArgsConstructor
-@Table(name = "call_status")
-public class CallStatus {
+@Table(name = "hospital_call_status")
+public class HospitalCallStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "admission_id", nullable = false)
-    Admission admission;
-
-    String hospitalName;
-
-    String hospitalPhoneNumber;
-
-    float distance;
-
-    int travelTime;
-
-    String detail; // TODO: 어떤 내용을 넣을 지 정하기
+    Long hospitalCallStatusId;
 
     @Enumerated(EnumType.STRING)
     CallStatus callStatus;
@@ -33,6 +20,4 @@ public class CallStatus {
     CallResponseStatus callResponseStatus;
 
     int callAttempts;
-
-    String ars_message;
 }
