1d0f2b20067b4857b84ed5eb029858783dcc47fd
jution113 <jution113@gmail.com>
2025-04-03 14:59:39 +0900

    chore: fix indent line

--- Avenir/src/main/java/juton113/Avenir/config/TwilioConfig.java
+++ Avenir/src/main/java/juton113/Avenir/config/TwilioConfig.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-    public class TwilioConfig {
-        @Value("${twilio.account-sid}")
-        private String accountSid;
+public class TwilioConfig {
+    @Value("${twilio.account-sid}")
+    private String accountSid;
 
-        @Value("${twilio.auth-token}")
-        private String authToken;
+    @Value("${twilio.auth-token}")
+    private String authToken;
 
-        @PostConstruct
+    @PostConstruct
     public void initTwilio() {
         Twilio.init(accountSid, authToken);
     }
