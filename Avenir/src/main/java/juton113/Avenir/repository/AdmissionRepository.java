2792f23b13a5c441fb854ccfbbba8d860b7d9578
jution113 <jution113@gmail.com>
2025-04-04 15:58:09 +0900

    feat: add AdmissionRepository

---
+++ Avenir/src/main/java/juton113/Avenir/repository/AdmissionRepository.java
@@ -0,0 +1,9 @@
+package juton113.Avenir.repository;
+
+import juton113.Avenir.domain.entity.Admission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdmissionRepository extends JpaRepository<Admission, Long> {
+}
