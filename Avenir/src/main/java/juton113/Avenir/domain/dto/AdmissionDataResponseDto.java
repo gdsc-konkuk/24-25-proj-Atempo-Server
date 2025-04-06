5b97a617c7a03c577121dba8fec10fb8aeab6ed4
jution113 <jution113@gmail.com>
2025-04-06 20:31:44 +0900

    feat: add constructor annotation to AdmissionDataResponseDto

--- Avenir/src/main/java/juton113/Avenir/domain/dto/AdmissionDataResponseDto.java
+++ Avenir/src/main/java/juton113/Avenir/domain/dto/AdmissionDataResponseDto.java
@@ -1,6 +1,7 @@
 package juton113.Avenir.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AdmissionDataResponseDto {
     @JsonProperty("hospital_list")
     private List<CreateHospitalResponseDto> hospitalList;
