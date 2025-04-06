60f3cf2dfc408bcc91a5bdd116c316bfa528b2de
jution113 <jution113@gmail.com>
2025-04-07 00:08:06 +0900

    feat: add say message to handleGather in TwilioController

--- Avenir/src/main/java/juton113/Avenir/controller/TwilioController.java
+++ Avenir/src/main/java/juton113/Avenir/controller/TwilioController.java
@@ -1,9 +1,13 @@
 package juton113.Avenir.controller;
 
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Hangup;
+import com.twilio.twiml.voice.Say;
 import juton113.Avenir.domain.dto.UpdateHospitalCallStatusDto;
 import juton113.Avenir.service.HospitalCallStatusService;
 import juton113.Avenir.service.TwilioService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +27,9 @@ public class TwilioController {
         return twilioService.createVoiceMessage(voiceMessage);
     }
 
-    @PostMapping("/gather-response")
-    public ResponseEntity<Void> handleGather(@RequestParam("Digits") String digit,
-                                             @RequestParam("CallSid") String callSid) {
+    @PostMapping(value = "/gather-response", produces = MediaType.APPLICATION_XML_VALUE)
+    public String handleGather(@RequestParam("Digits") String digit,
+                               @RequestParam("CallSid") String callSid) {
         UpdateHospitalCallStatusDto updateHospitalCallStatusDto = UpdateHospitalCallStatusDto.builder()
                 .callId(callSid)
                 .responseDigit(digit)
@@ -33,6 +37,11 @@ public class TwilioController {
 
         hospitalCallStatusService.updateHospitalCallStatus(updateHospitalCallStatusDto);
 
-        return ResponseEntity.ok().build();
+        VoiceResponse response = new VoiceResponse.Builder()
+                .say(new Say.Builder("Thank you for your cooperation with the call request. We are ending this call.").build())
+                .hangup(new Hangup.Builder().build())
+                .build();
+
+        return response.toXml();
     }
 }
