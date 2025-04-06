3bacd243c7e68356faf8b4f7a5740644d41dd501
jution113 <jution113@gmail.com>
2025-04-06 13:40:46 +0900

    feat: add id attrubute to AdmissionMessage entity

--- Avenir/src/main/java/juton113/Avenir/domain/entity/AdmissionMessage.java
+++ Avenir/src/main/java/juton113/Avenir/domain/entity/AdmissionMessage.java
@@ -1,10 +1,16 @@
 package juton113.Avenir.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 
+@Builder
 @Entity
 @Table(name = "admission_message")
 public class AdmissionMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long admissionMessageId;
+
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admission_id")
     private Admission admission;
