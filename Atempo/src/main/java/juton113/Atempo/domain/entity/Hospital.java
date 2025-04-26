dfc6bc23f050f83fb07800d40f6134e820324eb8
jution113 <jution113@gmail.com>
2025-04-27 01:04:54 +0900

    feat: add getter to hospitalId attribute in Hospital

--- Atempo/src/main/java/juton113/Atempo/domain/entity/Hospital.java
+++ Atempo/src/main/java/juton113/Atempo/domain/entity/Hospital.java
@@ -3,9 +3,8 @@ package juton113.Atempo.domain.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Builder
 @Entity
@@ -17,15 +16,22 @@ public class Hospital {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long hospitalId;
 
+    @Getter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "admission_id")
     private Admission admission;
 
+    @Getter
     private String name;
+    @Getter
     private String phoneNumber;
+    @Getter
     private String address;
+    @Getter
     private double distance;
+    @Getter
     private Integer travelTime;
+    @Getter
     private String departments;
 
     @OneToOne(mappedBy = "hospital")
