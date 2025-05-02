39e411bc67606cf180995bc3e8a41ff8956c8a3d
jution113 <jution113@gmail.com>
2025-05-02 17:03:03 +0900

    doc: Documantation Admission API

--- Atempo/src/main/java/juton113/Atempo/domain/dto/RetryAdmissionRequest.java
+++ Atempo/src/main/java/juton113/Atempo/domain/dto/RetryAdmissionRequest.java
@@ -1,6 +1,7 @@
 package juton113.Atempo.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import juton113.Atempo.domain.dto.common.Location;
 import lombok.*;
 
@@ -11,6 +12,7 @@ import lombok.*;
 public class RetryAdmissionRequest {
     private Location location;
 
+    @Schema(example = "5")
     @JsonProperty("search_radius")
     private int searchRadius;
 }
