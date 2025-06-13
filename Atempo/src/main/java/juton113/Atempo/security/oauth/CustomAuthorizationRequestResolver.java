7b9a6a703c13ace64a01cb9b280e710b8ec17cf4
jution113 <jution113@gmail.com>
2025-06-13 16:59:41 +0900

    feat: add extract call back uri func

---
+++ Atempo/src/main/java/juton113/Atempo/security/oauth/CustomAuthorizationRequestResolver.java
@@ -0,0 +1,41 @@
+package juton113.Atempo.security.oauth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
+public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
+
+    private final OAuth2AuthorizationRequestResolver defaultResolver;
+
+    public CustomAuthorizationRequestResolver(ClientRegistrationRepository repo, String baseUri) {
+        this.defaultResolver = new DefaultOAuth2AuthorizationRequestResolver(repo, baseUri);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request) {
+        OAuth2AuthorizationRequest req = defaultResolver.resolve(request);
+        return customize(req, request);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest resolve(HttpServletRequest request, String clientRegistrationId) {
+        OAuth2AuthorizationRequest req = defaultResolver.resolve(request, clientRegistrationId);
+        return customize(req, request);
+    }
+
+    private OAuth2AuthorizationRequest customize(OAuth2AuthorizationRequest req, HttpServletRequest request) {
+        if (req == null) return null;
+
+        String dynamicRedirectUri = request.getParameter("redirect_uri");
+        if (dynamicRedirectUri != null && !dynamicRedirectUri.isBlank()) {
+            return OAuth2AuthorizationRequest.from(req)
+                    .redirectUri(dynamicRedirectUri)
+                    .build();
+        }
+
+        return req;
+    }
+}
